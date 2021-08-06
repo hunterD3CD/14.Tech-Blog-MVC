5570c3a329c2f1ab64a606cf23e8a1300a40f010
hunterD3CD <hunterd3c@gmail.com>
2021-08-05 20:51:23 -0700

    create database and connect it using sequelize

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,10 @@
+// a module that loads environment variables from a .env file into process
 require("dotenv").config();
 
+// import the sequelize constructor from the library
 const Sequelize = require("sequelize");
 
+// create connection to our database, pass in your MySQL information for user info
   const sequelize = process.env.JAWSDB_URL
 ? new Sequelize(process.env.JAWSDB_URL)
 : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
