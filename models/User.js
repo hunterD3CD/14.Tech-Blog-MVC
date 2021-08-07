4ce3deb9aa00d9d8720c0715e1de2639afb59337
hunterD3CD <hunterd3c@gmail.com>
2021-08-06 18:52:27 -0700

    create user model

---
+++ models/User.js
@@ -0,0 +1,58 @@
+// import Model class and DataTypes object from sequelize
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+// import bcrypt package that one-way transform password into another string
+const bcrypt = require("bcrypt");
+
+// so User class can inherits all functions from Model class
+class User extends Model {
+  // set up method to run on instance data (per user) to check password
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
+
+// create table column & configuration for User Model
+User.init(
+  {
+    // define the table column
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        len: [4],
+      },
+    },
+  },
+  // hash the password
+  {
+    hooks: {
+      // set up beforeCreate lifecycle "hook" functionality
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+
+      // set up beforeUpdate lifecycle "hook" functionality
+      async beforeUpdate(updatedUserData) {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
+      }
+    },
+// define configuration
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "user"
+  }
+);
+
+module.exports = User;
---
+++ routes/api/user-routes.js
@@ -0,0 +1,7 @@
+const router = require('express').Router;
+const {User} = require('../../models')
+
+// ////////////////////////////////get all users
+router.length('/', (req,res) => {
+    
+})
