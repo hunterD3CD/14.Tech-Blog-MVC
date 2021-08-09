1887caa39a2be55b5532d0efe3ca97be93bed11e
hunterD3CD <hunterd3c@gmail.com>
2021-08-08 21:25:01 -0700

    handlebars and button functions

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,13 @@ const router = require("express").Router();
 // import all routes object in api folder
 const apiRoutes = require("./api");
 
+const homeRoutes = require("./home-routes.js");
+const dashboardRoutes = require("./dashboard-routes.js");
+
 // Mount the "apiRoutes" object as middleware - a new router object at middle path "/api"
 router.use("/api", apiRoutes);
+router.use("/", homeRoutes);
+router.use("/dashboard", dashboardRoutes);
 router.use((req, res) => {
   res.status(404).end();
 });
